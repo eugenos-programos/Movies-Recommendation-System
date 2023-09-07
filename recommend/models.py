d27ff365e635032c05c161335bb94f75bb22d552
eugenos-programos <elena.samchuk.73@mail.ru>
2023-09-07 17:59:03 +0300

    feat: add uploading movies

--- recommend/models.py
+++ recommend/models.py
@@ -4,7 +4,6 @@ from django.db import models
 # Create your models here.
 class Movie(models.Model):
     id = models.BigAutoField(name="id", primary_key=True)
-    title = models.CharField(name="name", max_length=100)
-    year = models.IntegerField(name="year")
-    genres = models.CharField(name="genres", max_length=200)
-
+    title = models.CharField(name="name", max_length=200)
+    year = models.IntegerField(name="year", blank=True, null=True)
+    genres = models.CharField(name="genres", max_length=100)
