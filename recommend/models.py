78c33d5980d0e832890cb5193b7faa44243708a0
eugenos-programos <elena.samchuk.73@mail.ru>
2023-09-07 15:01:55 +0300

    feat: add Movie model and make its migration

--- recommend/models.py
+++ recommend/models.py
@@ -1,3 +1,10 @@
+from typing import Any
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    id = models.BigAutoField(name="id", primary_key=True)
+    title = models.CharField(name="name", max_length=100)
+    year = models.IntegerField(name="year")
+    genres = models.CharField(name="genres", max_length=200)
+
